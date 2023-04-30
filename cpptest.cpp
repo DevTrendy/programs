5e4400f057cb17fadc8752fbdef83cdbc6c0a88b
devtrendy <devtrendyau@gmail.com>
2023-04-30 20:11:39 +1000

    Initial commit

---
+++ cpptest.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+    std::cout<<"Starting c++ development...!!!"<<std::endl;
+    
+    std::cout<<"Committing code to check git setup...!!!"<<std::endl;
+
+
+    return 0;
+}
